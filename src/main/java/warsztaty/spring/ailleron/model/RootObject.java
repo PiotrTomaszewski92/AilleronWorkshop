dae3dd05a7c0272c9b651939441db0aaf45d3530
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-07-27 14:50:19 +0200

    add annotation to set json property name

--- src/main/java/warsztaty/spring/ailleron/model/RootObject.java
+++ src/main/java/warsztaty/spring/ailleron/model/RootObject.java
@@ -1,6 +1,9 @@
 package warsztaty.spring.ailleron.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class RootObject {
+    @JsonProperty("RestResponse") // bez tego body rootResponse w Service zwraca null
     private RestResponse restResponse;
 
     public RestResponse getRestResponse() {
